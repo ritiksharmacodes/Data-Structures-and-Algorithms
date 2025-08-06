f5935aa450e27ac92af23382c3326632380f8ca1
ritiksharmacodes <ritikcan18@gmail.com>
2025-08-06 18:56:02 +0530

    changes

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -3,31 +3,28 @@ using namespace std;
 
 int main()
 {
-    string s = "pwwkew", str="";
-    int left = -1, maxe=0;
+    string s = "pwwkew", str = "", maxs = "";
 
-    for (int i = 0; i < s.length(); i++)
+    for (int right = 0; right < s.length(); right++)
     {
-        char c = s[i];
+        char c = s[right];
+
         int count = 0, j = 0;
         while (j < str.length())
         {
-            if (c != str[j])
-                count++;
-            else
+            if (c == str[j])
             {
-                left = i;
+                maxs = ( str.length() > maxs.length() )? str : maxs;
+                str="";
                 break;
             }
+            else count++;
             j++;
         }
-
-        if (count == str.length()) str = s.substr( left, (i-left)+1 );
-        
-        maxe = max( maxe, int(str.length()) );
+        if(count == str.length()) str += c;
     }
-    
-    cout << maxe << "  " << str ;
+
+    cout << maxs;
 
     return 0;
 }
